5c2095797e980186f4e674c18ad13b09f69fa515
urstrewly <iam0x191@gmail.com>
2018-09-16 23:02:25 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,6 @@
 import socket
 from datetime import datetime
 
-class client:
-    """ A simple client class """
-
-    HOST    = '127.0.0.1'
-    PORT    = 65222
-    VERSION = 'VERSION/1.0'
-
-    update = {import socket
-from datetime import datetime
-
 class auto_client:
     HOST    = '127.0.0.1'
     PORT    = 65222
@@ -48,28 +38,3 @@ client1 = auto_client()
 # regardless of new update, client always needs new software
 if (client1.connect_client()):
     print("Client has new software")
-
-        "Update":  0,
-        "Version": 0,
-        "Time": str(datetime.now())
-        
-    }
-
-    def connect_client(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
-            clientSocket.connect((self.HOST, self.PORT))
-            clientSocket.sendall(str.encode(self.VERSION))
-            data = clientSocket.recv(1024).decode()
-
-
-            print(data)
-
-
-
-
-
-
-client1 = client()
-
-
-client1.connect_client()
