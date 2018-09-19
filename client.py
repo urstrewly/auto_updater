dc53c19fcf2219bb23f5136d0c891b638795f758
urstrewly <iam0x191@gmail.com>
2018-09-19 15:17:44 -0400

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class auto_client:
     HOST    = '127.0.0.1'
     PORT    = 65222
-    VERSION = 'VERSION/0.0.1'
+    VERSION = 'VERSION/6.2.4'       # VERSION/6.2.4 -- newest version in text file to use as test
 
     update = {
         "Update":  0,
@@ -23,17 +23,15 @@ class auto_client:
                     
                     self.update["Update"]  = clientSocket.recv(1024).decode()
 
-            print("This clients current build: " + self.VERSION)        
-            print("Current build version: " + self.update["Version"])
-            print("update bool: " + self.update["Update"])        
+            print(self.update["Time"] + " -- This clients current build: " + self.VERSION)        
+            print(self.update["Time"] + " -- Current build version: " + self.update["Version"])
+            print(self.update["Time"] + " -- update bool: " + self.update["Update"])
 
             if (self.update["Update"] == '1'):
                 # run closeme.exe and close alpha.exe -- run new update.
-                print("client needs update")
                 return(True)
             
             else:
-                print("client doesnt need update")
                 return(True)
             
             
