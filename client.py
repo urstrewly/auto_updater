670e285c18d16d5a7d8c56c173ac86b054a2c31c
urstrewly <iam0x191@gmail.com>
2018-09-19 13:05:27 -0400

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class auto_client:
     HOST    = '127.0.0.1'
     PORT    = 65222
-    VERSION = 'VERSION/gay'
+    VERSION = 'VERSION/0.0.1'
 
     update = {
         "Update":  0,
@@ -18,14 +18,25 @@ class auto_client:
             if (clientSocket.connect((self.HOST, self.PORT))):
                 print("CANT CONNECT")
 
-            print(self.VERSION)
             if (self.send_version(clientSocket)):
-                    self.update["Update"] = clientSocket.recv(1024).decode()
-                    print(self.update["Update"])
+                    self.update["Version"] = clientSocket.recv(1024).decode()
+                    
+                    self.update["Update"]  = clientSocket.recv(1024).decode()
 
+            print("This clients current build: " + self.VERSION)        
+            print("Current build version: " + self.update["Version"])
+            print("update bool: " + self.update["Update"])        
+
+            if (self.update["Update"] == '1'):
+                # run closeme.exe and close alpha.exe -- run new update.
+                print("client needs update")
+                return(True)
+            
+            else:
+                print("client doesnt need update")
+                return(True)
             
             
-            return(True)
         return(False)
 
     def send_version(self, socket):           
@@ -36,5 +47,5 @@ class auto_client:
 client1 = auto_client()
 
 # regardless of new update, client always needs new software
-if (client1.connect_client()):
-    print("Client has new software")
+if not (client1.connect_client()):
+    print("Client cant connect")
